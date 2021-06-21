9d72ef36724fc23b59f2a461632616731c1bd049
Piyush <piyushpuniya00@gmail.com>
2021-06-21 16:31:40 +0530

    add cruds project

---
+++ index.js
@@ -0,0 +1,175 @@
+const nodemailer = require('nodemailer');
+const express = require("express");
+const app = express();
+// const port = 8000;
+const port = process.env.PORT;
+
+const fs = require("fs");
+const path = require("path");
+const mongoose = require('mongoose'),
+    autoIncrement = require('mongoose-auto-increment'); const { getMaxListeners } = require('process');
+;
+
+
+mongoose.connect("mongodb+srv://piyush:piyush2001@tabledata.k3zwz.mongodb.net/test", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+autoIncrement.initialize(db);
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+
+    console.log("connected to mongooose");
+
+});
+const loginschema = new mongoose.Schema({
+
+    id: String,
+    name: String,
+    phoneno: String,
+    email: String,
+    hobbies: String
+});
+
+loginschema.methods.speak = function () {//this is used in case you wanna have have method callback
+    const greeting = "data feeded of " + this.name
+    console.log(greeting);
+}
+
+loginschema.plugin(autoIncrement.plugin, { model: 'logindata', field: 'id' });
+
+const logindata = mongoose.model('logindata', loginschema);
+
+app.use("/static", express.static("static"));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+var id2 = mongoose.Types.ObjectId();;
+app.get("/", (req, res) => {
+
+    var id = req.query.id;
+    if (req.query.status == 'delete') {
+        dbdelete(id);
+        // showtable(res);
+
+    }
+    showtable(res);
+})
+
+app.post("/", (req, res) => {
+
+    console.log(req.body);
+    if (req.body.up_id == "") {
+        myname = req.body.name;
+        myphoneno = req.body.phoneno;
+        myemail = req.body.email;
+        myhobbies = req.body.hobbies;
+
+        const mydoc = new logindata({
+            name: myname,
+            phoneno: myphoneno,
+            email: myemail,
+            hobbies: myhobbies
+        });
+
+        // Inserts a new document with `name = 'Will Riker'` and
+        // `rank = 'Commander'`
+        mydoc.save();
+        res.status(200).render('indexpost');
+
+    } else if (req.body.up_id) {
+        myid = req.body.up_id;
+        myname = req.body.name;
+        myphoneno = req.body.phoneno;
+        myemail = req.body.email;
+        myhobbies = req.body.hobbies;
+        console.log("enetred")
+        logindata.updateOne({ id: `${myid}` }, {
+            $set: {
+                name: `${myname}`,
+                phoneno: `${myphoneno}`,
+                email: `${myemail}`,
+                hobbies: `${myhobbies}`
+            }
+        }, function (err, docs) {
+            if (err) console.log(err);
+
+        });
+    }
+    res.status(200).render('indexpost');
+
+})
+
+
+app.post("/:sendmail", (req, res) => {
+    console.log('mail send:  ', req.body);
+    res.status(200).render('indexpost');
+    var maildata = req.body.maildata;
+    console.log(typeof (maildata), maildata);
+    sendmail(maildata);
+});
+
+function showtable(res) {
+    logindata.find({}, function (err, docs) {
+        if (err) console.log(err);
+        var data = JSON.stringify(docs);
+        // console.log(docs)
+        var params = { "data": data };
+        res.status(200).render('index', params);
+
+    }).select('-_id -__v');
+}
+
+function dbdelete(strid) {
+    console.log("methods called", strid);
+    var myid = parseInt(strid);
+    // console.log(myid);
+    logindata.deleteOne({ id: `${strid}` }, function (err) {
+        if (err) return console.log(err);
+
+    });
+}
+
+
+function sendmail(mytext) {
+    var string = "G-Mail Sent";
+    var transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'piyushpuniya2001@gmail.com',
+            pass: 'frvgerygbdwgdprk'
+        }
+    });
+    console.log("sending---");
+    const message = {
+        from: 'piyushpuniya2001@gamil.com',
+        // to: `info@redpositive.in`,
+        to: 'piyushpuniya00@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: `${mytext}`,
+        // attachments: [
+        //     { // Use a URL as an attachment
+        //         filename: 'your-attachment.png',
+        //         path: 'https://media.gettyimages.com/photos/view-of-tesla-model-s-in-barcelona-spain-on-september-10-2018-picture-id1032050330?s=2048x2048'
+        //     }
+        // ]
+
+    };
+    console.log("sending---");
+    transport.sendMail(message, function (err, info) {
+        if (err) {
+            console.log(err)
+            string = "Some err occured plz try after some time";
+        }
+        console.log("mail infi", info);
+        string = "Email Sent! Check Now.";
+
+    });
+    return string;
+}
+
+app.listen(port, () => {
+    console.log(`connection started at port http://127.0.0.1:${port}/`)
+});
---
+++ static/script.js
@@ -0,0 +1,147 @@
+var data = document.getElementById("helper").getAttribute("data");
+data = JSON.parse(data);
+console.log(data);
+var count = Object.keys(data).length;
+console.log(count);
+console.log(data);
+
+
+var tbody = document.getElementById("tbody");
+
+for (x in data) {
+    var tr = document.createElement("tr");
+
+    var td0 = document.createElement("td");
+
+    var sendbox = document.createElement("input");
+    sendbox.type = "checkbox";
+    sendbox.setAttribute("id", "sendM" + data[x]["id"])
+    sendbox.onclick = function () {
+        savedata(this.id);
+    }
+
+    td0.appendChild(sendbox);
+
+    tr.appendChild(td0);
+
+    var td1 = document.createElement("td");
+    td1.innerHTML = data[x]["id"];
+    tr.appendChild(td1);
+
+    var td2 = document.createElement("td");
+    td2.innerHTML = data[x]["name"];
+    td2.setAttribute("id", "name" + data[x]["id"])
+
+    tr.appendChild(td2);
+
+    var td3 = document.createElement("td");
+    td3.innerHTML = data[x]["email"];
+    td3.setAttribute("id", "email" + data[x]["id"])
+
+    tr.appendChild(td3);
+
+    var td4 = document.createElement("td");
+    td4.innerHTML = data[x]["phoneno"];
+    td4.setAttribute("id", "phoneno" + data[x]["id"])
+
+    tr.appendChild(td4);
+
+    var td5 = document.createElement("td");
+    td5.innerHTML = data[x]["hobbies"];
+    td5.setAttribute("id", "hobbies" + data[x]["id"])
+
+    tr.appendChild(td5);
+
+    var td6 = document.createElement("td");
+
+    var updatebtn = document.createElement("button");
+    updatebtn.classList.add("btn");
+    updatebtn.classList.add("btn-warning");
+    updatebtn.innerHTML = "Update";
+    updatebtn.style.margin = "0pc 10px 0px 0px";
+    updatebtn.setAttribute("id", "up" + data[x]["id"])
+    updatebtn.setAttribute("data-toggle", "modal");
+    updatebtn.setAttribute("data-target", "#myModal");
+
+    updatebtn.onclick = function () {
+        updatedata(this.id);
+    }
+    td6.appendChild(updatebtn);
+
+    var deletebtn = document.createElement("button");
+    deletebtn.classList.add("btn");
+    deletebtn.classList.add("btn-danger");
+    deletebtn.innerHTML = "Delete";
+    deletebtn.setAttribute("id", "de" + data[x]["id"])
+    deletebtn.onclick = function () {
+        httpGetAsync(this.id);
+    }
+    td6.appendChild(deletebtn);
+
+    tr.appendChild(td6);
+
+    tbody.appendChild(tr);
+}
+
+function httpGetAsync(debtnid) {
+    console.log(debtnid);
+    var id = debtnid.slice(2,);
+    window.location.href = "http://127.0.0.1:8000/?id=" + id + "&status=delete";
+}
+
+function updatedata(upbtnid) {
+    console.log("upbtnid", upbtnid);
+    var id = upbtnid.slice(2,);
+    var myname = document.getElementById("name" + id).innerHTML;
+    var myphoneno = document.getElementById("phoneno" + id).innerHTML;
+    var myemail = document.getElementById("email" + id).innerHTML;
+    var myhobbies = document.getElementById("hobbies" + id).innerHTML;
+
+    document.getElementById("up_id").value = id;
+    document.getElementById("name").value = myname;
+    document.getElementById("email").value = myemail;
+    document.getElementById("phoneno").value = myphoneno;
+    document.getElementById("hobbies").value = myhobbies;
+    document.getElementById("subbtn").innerHTML = "Update";
+    document.getElementById("formhead").innerHTML = "UPDATE DATA OF TABLE";
+
+
+}
+
+var dataToBeSend = {
+
+};
+function savedata(senderId) {
+    var id = senderId.slice(5,);
+    console.log("senderId", id);
+
+    var myname = document.getElementById("name" + id).innerHTML;
+    var myphoneno = document.getElementById("phoneno" + id).innerHTML;
+    var myemail = document.getElementById("email" + id).innerHTML;
+    var myhobbies = document.getElementById("hobbies" + id).innerHTML;
+    var adddata = {
+        "name": `${myname}`,
+        "email": `${myemail}`,
+        "phone": `${myphoneno}`,
+        "hobbies": `${myhobbies}`,
+    };
+    var datas = "data" + id;
+
+    if (document.getElementById(senderId).checked != false) {
+        dataToBeSend[datas] = adddata;
+    } else {
+        delete dataToBeSend[datas];
+    }
+    document.getElementById("maildata").value = JSON.stringify(dataToBeSend)
+    // console.log(JSON.stringify(dataToBeSend));
+}
+
+if(!window.location.hash.includes("#freshpage")) {
+    window.location.href += "#freshpage";
+    window.location.reload()
+}
+
+
+
+
+
